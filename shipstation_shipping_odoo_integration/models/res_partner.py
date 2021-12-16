9061529259f050f9437c737a63e2f8d6e78d59f7
topherbeckwork <92904037+topherbeckwork@users.noreply.github.com>
2021-12-16 22:03:22 +0800

    Removing Purchased App
    Removing the purchased app out of our development branch so that I can test our my new app.

--- shipstation_shipping_odoo_integration/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields,models,api,_
-import logging
-_logger = logging.getLogger(__name__)
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-    
-    imported_from_shipstation = fields.Boolean(string='Imported From ShipStation')
-    shipstation_customerId = fields.Char(string='Shipstation Customer ID')
