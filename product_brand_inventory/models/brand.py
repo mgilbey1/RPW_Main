a71579b3a652532a272af6ce63690ef9b9d09d55
John Beck <topherbeckwork@gmail.com>
2022-03-10 17:11:43 +0800

    brand fix

--- product_brand_inventory/models/brand.py
+++ product_brand_inventory/models/brand.py
@@ -27,21 +27,21 @@ class ProductBrand(models.Model):
 
     brand_id = fields.Many2one('product.brand',string='Brand')
 
+
 class BrandProduct(models.Model):
     _name = 'product.brand'
     _description = 'Brand Name'
 
-    name = fields.Char("Name")
+    name = fields.Char(String="Name")
     brand_image = fields.Binary()
-    member_ids = fields.One2many("product.template", "brand_id")
-    product_count = fields.Char("Product Count", compute="get_count_products", store=True)
+    member_ids = fields.One2many('product.template', 'brand_id')
+    product_count = fields.Char(String='Product Count', compute='get_count_products', store=True)
 
     @api.depends('member_ids')
     def get_count_products(self):
         self.product_count = len(self.member_ids)
 
-
 class BrandReportStock(models.Model):
     _inherit = 'stock.quant'
 
-    brand_id = fields.Many2one("Brand", related='product_id.brand_id', store=True, readonly=True)
+    brand_id = fields.Many2one(related='product_id.brand_id', string='Brand', store=True, readonly=True)
