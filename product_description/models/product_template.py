20d860723a36b9d0c559b53af4b332101f3dceef
John Beck <topherbeckwork@gmail.com>
2022-03-16 19:47:17 +0800

    update

--- product_description/models/product_template.py
+++ product_description/models/product_template.py
@@ -7,4 +7,4 @@ class ProductTemplateWebsiteDescription(models.Model):
     _inherit = 'product.template'
 
     
-    web_description = fields.Text('Website Description', help="This description will show up on website as product description.")
+    web_description = fields.Text('Short Description', help="This description will show up on website as product description.")
